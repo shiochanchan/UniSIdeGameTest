553b18f2de5873f53ec89fe6cbd12a22c208ab44
MUG030 <ys8267928563@outlook.jp>
2023-01-29 11:17:34 +0900

    1/28 進捗
    アニメーション関係

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,16 +20,42 @@ public class PlayerController : MonoBehaviour
     /// <summary> 地面に立っているフラグ </summary>
     bool _onGround = false;
 
+    /// <summary>
+    /// アニメーション対応
+    /// </summary>
+    Animator animator;
+    public string stopAnime = "PlayerStop";
+    public string moveAnime = "PlayerMove";
+    public string jumpAnime = "PlayerJump";
+    public string goalAnime = "PlayerClear";
+    public string deadAnime = "PlayerGameOver";
+    string _nowAnime = "";
+    string _oldAnime = "";
+
+    /// <summary> ゲームの状態 </summary>
+    public static string _gameState = "playing";
+
     // Start is called before the first frame update
     void Start()
     {
         // Rigidbody2Dをとる
         rbody = GetComponent<Rigidbody2D>();
+        // Animatorを取ってくる
+        animator = GetComponent<Animator>();
+        _nowAnime = stopAnime;
+        _oldAnime = stopAnime;
+        // ゲーム中にする
+        _gameState = "playing";
     }
 
     // Update is called once per frame
     void Update()
     {
+        // ゲーム中以外の判定でメソッド内の以下を実行しない
+        if(_gameState != "playing")
+        {
+            return;
+        }
         //　水平方向の入力を確認する
         _axisH = Input.GetAxisRaw("Horizontal");
         //　向きの調整
@@ -56,8 +82,13 @@ public class PlayerController : MonoBehaviour
     /// <summary>
     /// 速度更新
     /// </summary>
-    private void FixedUpdate()
+    void FixedUpdate()
     {
+        // ゲーム中以外はメソッド内の以下の判定を実行しない
+        if (_gameState != "playing")
+        {
+            return;
+        }
         // 地上判定
         _onGround = Physics2D.Linecast(transform.position, transform.position - (transform.up * 0.1f), groundLayer);
         // 地面の上もしくは速度が0ではない
@@ -75,6 +106,29 @@ public class PlayerController : MonoBehaviour
             rbody.AddForce(jumpPw, ForceMode2D.Impulse);    //瞬間的な力を加える
             _gojump = false;
         }
+        // 地上にいるとき
+        if (_onGround)
+        {
+            if (_axisH == 0)
+            {
+                _nowAnime = stopAnime;
+            }
+            else
+            {
+                _nowAnime = moveAnime;
+            }
+        }
+        else
+        {
+            _nowAnime = jumpAnime;
+        }
+
+        // アニメーション再生
+        if(_nowAnime != _oldAnime)
+        {
+            _oldAnime = _nowAnime;
+            animator.Play(_nowAnime);
+        }
     }
 
     /// <summary>
@@ -86,4 +140,61 @@ public class PlayerController : MonoBehaviour
         Debug.Log("ジャンプボタンをおした");
     }
 
+    /// <summary>
+    /// 接触開始
+    /// </summary>
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Goal")
+        {
+            // ゴールメソッドを呼ぶ
+            Goal();
+        }
+        else if (collision.gameObject.tag == "Dead")
+        {
+            // ゲームオーバーメソッドを呼ぶ
+            GameOver();
+        }
+    }
+
+    /// <summary>
+    /// ゴールメソッド
+    /// </summary>
+    public void Goal()
+    {
+        animator.Play(goalAnime);
+
+        _gameState = "gameclear";
+        // ゲーム停止メソッドを呼ぶ
+        GameStop();
+    }
+
+    /// <summary>
+    /// ゲームオーバーメソッド
+    /// </summary>
+    public void GameOver()
+    {
+        animator.Play(deadAnime);
+
+        Debug.Log("しんだ");
+        _gameState = "gameover";
+        // ゲーム停止メソッドを呼ぶ
+        GameStop();
+        // ゲームオーバー演出
+        // プレイヤーの当たり判定を消す
+        GetComponent<CapsuleCollider2D>().enabled = false;
+        // プレイヤーを少し上に上げる
+        rbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+    }
+
+    /// <summary>
+    /// ゲーム停止メソッド
+    /// </summary>
+    void GameStop()
+    {
+        // Rigidbody2Dをとってくる
+        Rigidbody2D rbody = GetComponent<Rigidbody2D>();
+        // 速度0で強制停止
+        rbody.velocity = new Vector2(0, 0);
+    }
 }
